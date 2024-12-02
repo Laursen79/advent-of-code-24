11487fe6137de2871c4e7514c941f3d7530de5e2
Simon Deleuran Laursen <69715502+Laursen79@users.noreply.github.com>
2024-12-02 18:09:42 +0100

    updated day 2

--- src/advent_of_code_24/days/day_2/__init__.py
+++ src/advent_of_code_24/days/day_2/__init__.py
@@ -1,9 +1,7 @@
 import copy
 from collections import Counter
 from functools import cache
-from itertools import count
 from pathlib import Path
-from sys import stdout
 
 
 @cache
@@ -27,10 +25,25 @@ def is_safe(report: list[int]) -> bool:
             return False
     return True
 
+def is_safe_fast(report: list[int]) -> bool:
+    prev_diff = 0
+    for i in range(len(report) - 1):
+        # Rule 1: strictly increasing or decreasing.
+        diff = report[i + 1] - report[i]
+        if not prev_diff ^ diff >= 0 and i != 0:
+            return False
+        prev_diff = diff
+
+        # Rule 2: diff must be at least 1 and at most 3
+        if not 0 < abs(diff) <= 3:
+            return False
+
+    return True
+
 
 def task_1():
     reports = get_reports()
-    safe_count = Counter(map(is_safe, reports))[True]
+    safe_count = Counter(map(is_safe_fast, reports))[True]
     print(safe_count)
     # 526
 
@@ -39,7 +52,7 @@ def problem_damper(report: list[int]) -> bool:
     for i in range(len(report)):
         clone = copy.deepcopy(report)
         clone.pop(i)
-        if is_safe(clone):
+        if is_safe_fast(clone):
             return True
 
     return False
